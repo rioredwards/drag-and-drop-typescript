1692a284071498457e4aad7efdf7bef883d6a5ab
Rio Edwards <rioredwards@gmail.com>
2023-03-14 16:54:17 -0700

    Using a getter to return a phrase with plural/singular

--- src/app.ts
+++ src/app.ts
@@ -169,6 +169,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} people`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
     this.project = project;
@@ -183,8 +191,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     console.log("this.element: ", this.element);
     console.log("this.project.title: ", this.project.title);
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
