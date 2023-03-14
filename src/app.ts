51ff0160073561be4adc1980e02c4c20139f1749
Rio Edwards <rioredwards@gmail.com>
2023-03-13 18:36:33 -0700

    Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
+// This class defines the form for adding a new project
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLTextAreaElement;
+  peopleInputElement: HTMLInputElement;
 
+  // Get DOM elements (from within <template>), configure and render to DOM
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
       document.getElementById("project-input")!
@@ -14,9 +19,35 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLTextAreaElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  // Triggers when form submitted
+  // Broken because "this" is not bound to the class when called by eventListener
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  // Configure event listeners
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  // Attach to DOM
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
