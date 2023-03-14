077edfa70fd892076e1e35662b9c1c6eb24dd97c
Rio Edwards <rioredwards@gmail.com>
2023-03-14 00:29:41 -0700

    Add projectList class and render to DOM

--- src/app.ts
+++ src/app.ts
@@ -58,7 +58,41 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
-//SECTION - ProjectList Class
+// ANCHOR - ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished" = "active") {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-list")!
+    );
+    this.hostElement = <HTMLDivElement>document.getElementById("app")!;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+//SECTION - ProjectInput Class
 // This class defines the form for adding a new project
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -164,3 +198,5 @@ class ProjectInput {
 // !SECTION
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
