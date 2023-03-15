651fab88b2f7d06cc90664f0a08221189ab546bf
Rio Edwards <rioredwards@gmail.com>
2023-03-14 22:42:40 -0700

    Finish splitting up files into different folders

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,15 @@
+namespace App {
+  // -- Autobind Decorator
+  // Autobind decorator: Method decorator: (binds "this" to the class)
+  export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
---
+++ src/state/project-state.ts
@@ -0,0 +1,61 @@
+namespace App {
+  // -- Listener Type
+  type Listener<T> = (items: T[]) => void;
+
+  // -- State Class
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  // -- Project State Management
+  export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    // Singleton pattern: Only one instance of this class can exist
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        numOfPeople,
+        ProjectStatus.Active
+      );
+      this.projects.push(newProject);
+      this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const project = this.projects.find((prj) => prj.id === projectId);
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  // -- Global instance of Project State
+  export const projectState = ProjectState.getInstance();
+}
